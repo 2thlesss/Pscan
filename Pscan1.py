f72959898863a81a38d877e01aac630aa318e8ca
2thlesss <thomas.jadie.reeves@gmail.com>
2023-10-30 13:59:43 -0400

    added a timeout

--- Pscan1.py
+++ Pscan1.py
@@ -4,6 +4,8 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+socket.setdefaulttimeout(2)
+
 host = input("[*] Enter the host to scan: ")
 
 port = int(input("[*] Enter the port to scan:"))
