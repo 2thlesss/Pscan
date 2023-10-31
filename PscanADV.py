c0b8672f018a33f6c18ad9a0bec556f96c64e124
2thlesss <thomas.jadie.reeves@gmail.com>
2023-10-31 10:27:52 -0400

    this course is in python2, i have updated the formatting for python 3.6 and later

--- PscanADV.py
+++ PscanADV.py
@@ -7,38 +7,49 @@ import optparse
 # import everything
 from threading import *
 
+#connection scanner
+def connectionScan(targetHost, targetPort):
+        try:
+                sock = socket(AF_INET, SOCK_STREAM)
+                sock.connect((targetHost, targetPort))
+                print('[+] {}:{} Open'.format(targetHost, targetPort))
+        except:
+                print('[-] {}:{} Closed'.format(targetHost, targetPort))
+        finally:
+                sock.close()
+
 #portscanner function
-def portScan(targetHost, targetPort):
-	#resolve domain name to IP address
-	try:
-		targetIP = gethostbyname(targetHost)
-	except:
-		print ('Cant Resolve Target Host %s ' %targetHost)
-	try:
-		targetName = gethostbyaddr(targetIP)
-		print ('[+] Scan Results For: ' + targetName[0])
-	except:
-		print ('[+] Scan Results for: '+ targetIP) 
-	setdefaulttimeout(1)
-	for targetPort in targetPort:
-		t = Thread(target=connectionScan, args = (targetHost,int(targetPort)))
-		t.start()
+def portScan(targetHost, targetPorts):
+        #resolve domain name to IP address
+        try:
+                targetIP = gethostbyname(targetHost)
+        except:
+                print('Cant Resolve Target Host {}'.format(targetHost))
+        try:
+                targetName = gethostbyaddr(targetIP)
+                print('[+] Scan Results For: ' + targetName[0])
+        except:
+                print('[+] Scan Results for: '+ targetIP) 
+        setdefaulttimeout(1)
+        for targetPort in targetPorts:
+                t = Thread(target=connectionScan, args=(targetHost, int(targetPort)))
+                t.start()
 
 #main function
 def main():
-	#call options for user
-	parser = optparse.OptionParser('Usage of Program: ' + '-H <target host> -p <target port>')
-	parser.add_option('-H', dest='targetHost', type = 'string', help= 'specify target host')
-	parser.add_option('-p', dest='targetPort', type = 'string', help = 'specify target ports seperated by a comma')
-	(options, args) = parser.parse_args()
-	targetHost = options.targetHost
-	#targetPort set to accept comma seperated input
-	targetPort = str(options.targetPort).split(',')
-	if (targetHost == None) | (targetPort[0] == None):
-		print (parser.usage)
-		exit (0)
-	portScan(targetHost, targetPort)
+        #call options for user
+        parser = optparse.OptionParser('Usage of Program: ' + '-H <target host> -p <target port>')
+        parser.add_option('-H', dest='targetHost', type='string', help='specify target host')
+        parser.add_option('-p', dest='targetPort', type='string', help='specify target ports separated by commas')
+        (options, args) = parser.parse_args()
+        targetHost = options.targetHost
+        #targetPort set to accept comma-separated input
+        targetPorts = str(options.targetPort).split(',')
+        if (targetHost is None) or (targetPorts[0] is None):
+                print(parser.usage)
+                exit(0)
+        portScan(targetHost, targetPorts)
 
-if __name__ = '__main__' :
+if __name__ == '__main__':
 
-	main()
+        main()
