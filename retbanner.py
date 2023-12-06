8921c9456a3e3b9f30510ea433267f8e3e703307
2thlesss <thomas.jadie.reeves@gmail.com>
2023-12-06 16:24:25 -0500

    changed timeout to 2. will only display errors when timed out. scans the first 1000 ports

--- retbanner.py
+++ retbanner.py
@@ -4,28 +4,30 @@ import socket
 
 def retBanner(ip, port):
     try:
-        timeout = 5  # Set the timeout to 5 seconds
+        timeout = 2  # Set the timeout to 2 seconds
         socket.setdefaulttimeout(timeout)
         s = socket.socket()
         s.connect((ip, port))
         banner = s.recv(1024)
         return banner
+    except ConnectionRefusedError:
+        return None  # Ignore "Connection refused" errors
     except Exception as e:
         print("Error: " + str(e))
-        return
+        return None
 
 def main():
-    port = 22
-    ip = "192.168.1.108"
-    timeout = 5  # Set the timeout to 5 seconds
-    print(f"Scanning {ip}:{port} with a timeout of {timeout} seconds...")
-    for remaining in range(timeout, 0, -1):
-        print(f"Timeout in {remaining} seconds...", end='\r')
+    ip = input('[*] Enter target IP: ')
+    print(f"Scanning {ip}...")
+    for port in range(1, 1001):  # Scan ports from 1 to 1000
         banner = retBanner(ip, port)
         if banner:
-            print(f"[+]{ip}:{port}: {banner.decode('utf-8')}")
-            break
-    else:
-        print("Timeout reached. No response.")
+            try:
+                decoded_banner = banner.decode('utf-8')
+                decoded_banner = decoded_banner.strip()  # Strip newline characters
+            except UnicodeDecodeError:
+                decoded_banner = banner.decode('latin-1')  # Try using latin-1 encoding if utf-8 fails
+                decoded_banner = decoded_banner.strip()  # Strip newline characters
+            print(f"[+]{ip}:{port}: {decoded_banner}")
 
 main()
